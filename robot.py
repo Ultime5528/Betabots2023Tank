1d78501a418eda4ec48057971630f8084b646532
Henri Martin <4001624@keranna.qc.ca>
2023-10-25 17:27:30 -0400

    cleaned useless line

---
+++ robot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+import math
+from typing import Optional
+
+import commands2
+import wpilib
+
+
+
+
+class Robot(commands2.TimedCommandRobot):
+    def robotInit(self):
+        #self.stick = commands2.button.CommandJoystick(0)
+        self.autoChooser = wpilib.SendableChooser()
+
+    def autonomousInit(self) -> None:
+        self.autoCommand: commands2.CommandBase = self.autoChooser.getSelected()
+        if self.autoCommand:
+            self.autoCommand.schedule()
+
+    def teleopInit(self) -> None:
+        if self.autoCommand:
+            self.autoCommand.cancel()
+
+    def robotPeriodic(self) -> None:
+        super().robotPeriodic()
+
+
+if __name__ == "__main__":
+    wpilib.run(Robot)
