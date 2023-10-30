7b05ff10b408d1e9ebe1a24c7b23b0b291b2ed16
Henri Martin <4001624@keranna.qc.ca>
2023-10-30 17:09:04 -0400

    actualise drivetrain

--- Drivetrain.py
+++ Drivetrain.py
@@ -1,24 +1,23 @@
 from utils.sparkmaxutils import configure_leader, configure_follower, rev
 import wpilib
 import wpilib.drive
+import commands2
 
 
 class Robot(wpilib.TimedRobot):
     def robotInit(self):
         self._motor_left = rev.CANSparkMax(3, rev.CANSparkMax.MotorType.kBrushless)
-        configure_leader(self._motor_left, "brake")
+       
 
         self._motor_left_follower = rev.CANSparkMax(4,
                                                     rev.CANSparkMax.MotorType.kBrushless)
-        configure_follower(self._motor_left_follower, self._motor_left, "brake")
+
 
         self._motor_right = rev.CANSparkMax(1,
                                             rev.CANSparkMax.MotorType.kBrushless)
-        configure_leader(self._motor_right, "brake")
 
         self._motor_right_follower = rev.CANSparkMax(2,
                                                      rev.CANSparkMax.MotorType.kBrushless)
-        configure_follower(self._motor_right_follower, self._motor_right, "brake")
 
         self._drive = wpilib.drive.DifferentialDrive(self._motor_left, self._motor_right)
         wpilib.SmartDashboard.putData("Drive", self._drive)
