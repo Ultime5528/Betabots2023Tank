0792c4309ff558b923861295702fd9f883ebf1f5
Henri Martin <4001624@keranna.qc.ca>
2023-11-27 17:33:13 -0500

    change joystick to Xbox Controller

--- commands/drive.py
+++ commands/drive.py
@@ -7,14 +7,14 @@ from utils.safecommand import SafeCommand
 
 
 class Drive(SafeCommand):
-    def __init__(self, drivetrain: Drivetrain, stick: wpilib.Joystick):
+    def __init__(self, drivetrain: Drivetrain, xboxremote: wpilib.XboxController):
         super().__init__()
-        self.stick = stick
+        self.xboxremote = xboxremote
         self.drivetrain = drivetrain
         self.addRequirements(drivetrain)
 
     def execute(self):
-        self.drivetrain.drive(self.stick.getY() * -1, self.stick.getX() * -1)
+        self.drivetrain.drive(self.xboxremote.getLeftY() * -1, self.xboxremote.getLeftX() * -1)
 
     def end(self, interrupted: bool) -> None:
         self.drivetrain.drive(0.0, 0.0)
--- robot.py
+++ robot.py
@@ -4,7 +4,7 @@ from typing import Optional
 
 import commands2
 import wpilib
-from commands2._impl.button import CommandJoystick
+from commands2._impl.button import CommandJoystick, CommandXboxController
 
 from commands.drive import Drive
 from subsystems.drivetrain import Drivetrain
@@ -13,10 +13,12 @@ from subsystems.drivetrain import Drivetrain
 class Robot(commands2.TimedCommandRobot):
     def __init__(self):
         super().__init__()
-        self.stick = CommandJoystick(0)
+        self.xboxremote = CommandXboxController(0)
+        #self.stick = CommandJoystick(1)
         self.drivetrain = Drivetrain()
 
-        self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))
+        # self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.stick))  # À commenter si on veut la manette.
+        self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.xboxremote))  # À commenter si on veut le joystick.
 
 
 
