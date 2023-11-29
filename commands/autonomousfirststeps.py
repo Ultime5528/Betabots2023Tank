7660be20889fd99e6a2129d53604de56fde6a347
Felix Campeau <4000329@keranna.qc.ca>
2023-11-29 18:09:02 -0500

    Progress on autonomous modes

---
+++ commands/autonomousfirststeps.py
@@ -0,0 +1,10 @@
+import commands2
+
+from commands import launch
+#from commands.movearm import MoveArm
+
+class AutonomousFirstSteps(commands2.SequentialCommandGroup):
+    def __init__(self):
+        super().__init__(
+            
+        )
