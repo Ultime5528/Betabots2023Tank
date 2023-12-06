56e7c298df5a38e98db396d007bfd20d7f14b3fb
Henri Martin <4001624@keranna.qc.ca>
2023-12-06 17:42:34 -0500

    Fix adding commands to buttons

--- robot.py
+++ robot.py
@@ -3,35 +3,41 @@ import math
 from typing import Optional
 import commands2
 import wpilib
-from commands2._impl.button import CommandJoystick, CommandXboxController
+from commands2._impl.button import CommandXboxController
 
 from commands.avancerx import AvancerX
 from commands.drive import Drive
 from commands.launch import Launch
 from commands.movearm import MoveArm
 from commands.resetarm import ResetArm
+from subsystems.arm import Arm
 from subsystems.drivetrain import Drivetrain
+from subsystems.launcher import Launcher
 
 
 class Robot(commands2.TimedCommandRobot):
     def __init__(self):
         super().__init__()
         self.xboxremote = CommandXboxController(0)
-        
+        self.arm = Arm()
+
+        self.launcher = Launcher()
         self.drivetrain = Drivetrain()
         self.drivetrain.setDefaultCommand(Drive(self.drivetrain, self.xboxremote))
         
         self.autoChooser = wpilib.SendableChooser()
         self.autoCommand: Optional[commands2.CommandBase] = None
 
+        self.setupButtons()
+
         wpilib.SmartDashboard.putData("AvancerX", AvancerX(self.drivetrain, 0.5, 0.75))
 
-
     def setupButtons(self):
-        self.xboxremote.button(10).onTrue(Launch(self.lauch))
-        self.xboxremote.button(4).onTrue(MoveArm(self.tolevel2))
-        self.xboxremote.button(1).onTrue(ResetArm(self.resetarm))
-        self.xboxremote.button(2).onTrue(MoveArm(self.tolevel1))
+        self.xboxremote.button(10).onTrue(Launch(self.launcher))
+        self.xboxremote.button(4).onTrue(MoveArm.toLevel2(self.arm))
+        self.xboxremote.button(1).onTrue(ResetArm(self.arm))
+        self.xboxremote.button(2).onTrue(MoveArm.toLevel1(self.arm))
+
     def autonomousInit(self) -> None:
         self.autoCommand: commands2.CommandBase = self.autoChooser.getSelected()
         if self.autoCommand:
