0b01b9ea113533f964a657f062059a16ee6d12ae
Henri Martin <4001624@keranna.qc.ca>
2023-12-06 16:29:52 -0500

    Draft tournerx

---
+++ commands/tournerx.py
@@ -0,0 +1,25 @@
+import math
+
+from subsystems.drivetrain import Drivetrain
+from utils.safecommand import SafeCommand
+
+
+class TournerX(SafeCommand):
+    def __init__(self, drive: Drivetrain, angle: float, speed: float):
+        self.setName("TournerX")
+        super().__init__()
+        self.angle = angle
+        self.drive = drive
+        self.speed = speed
+        self.addRequirements(self.drive)
+        self.error = float('inf')
+
+    def initialize(self):
+        self.drive.resetOdometry()
+
+    def execute(self):
+        self.error = self.drive.getAngle() - self.angle
+
+
+    def isFinished(self) -> bool:
+        return abs(self.error) <= 3
